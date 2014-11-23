351e1db0febac8050906c075e39ea4371e983780
Pablo Ismael Garcia Murillo <pablo.ismael90@gmail.com>
2014-11-23 13:23:12 -0600

    Cambiando base de dato

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'grupomesa',
+			'database'  => 'localhost',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
